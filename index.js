ebb4507d7a901130e0703c09cfa58d04a1f1aae2
BIPLOB <biplob2358@gmail.com>
2022-03-03 20:44:35 +0600

    add

---
+++ index.js
@@ -0,0 +1,15 @@
+var name = window.prompt("Enter The Teacher Initial :");
+console.log(name);
+switch (name) {
+  case "MIJ":
+    console.log(" Prof. Dr. Md. Ismail Jabiullah");
+    break;
+
+  case "SRH":
+    console.log("Sheikh Rashed Haider Noori");
+    break;
+
+  case "ZH":
+    console.log("Zahid Hasan");
+    break;
+}
